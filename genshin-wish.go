71cc5dd5b70953314ced0fdbd48bcdaa79b0564b
Binbin Ye <phantom2501@gmail.com>
2021-01-19 23:25:16 +0900

    Add csv export option

--- genshin-wish.go
+++ genshin-wish.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -9,6 +10,8 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"path"
+	"path/filepath"
 	"sort"
 	"strconv"
 
@@ -377,6 +380,33 @@ func (p *GenshinWishParser) PrintStatistics() {
 	}
 }
 
+func (p *GenshinWishParser) ExportGachaLog(filePath string) error {
+	file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	log.Infof("正在保存到%s ...", filePath)
+
+	err = file.Truncate(0)
+	if err != nil {
+		return nil
+	}
+
+	writer := csv.NewWriter(file)
+	writer.Write([]string{"UID", "物品ID", "物品名", "稀有度", "卡池", "抽卡时间"})
+	for _, gachaConfig := range p.Configs {
+		gachaLogs := p.GachalLogInPool[gachaConfig.Key]
+		for _, gachaLog := range gachaLogs {
+			itemInfo := p.ItemTable[gachaLog.ItemID]
+			writer.Write([]string{gachaLog.UID, itemInfo.ItemID, itemInfo.Name, itemInfo.RankType, gachaConfig.Name, gachaLog.Time})
+		}
+	}
+
+	writer.Flush()
+	return nil
+}
+
 func main() {
 	formatter := logging.MustStringFormatter("%{color}%{time:2006-01-02T15:04:05.000000-07:00} %{module} [%{level}] <%{pid}> [%{shortfile} %{shortfunc}] %{message}%{color:reset}")
 	logging.SetBackend(logging.NewBackendFormatter(logging.NewLogBackend(os.Stderr, "", 0), formatter))
@@ -424,4 +454,14 @@ func main() {
 	parser.MakeStatistics()
 	log.Debug(parser.StatisticsInPool)
 	parser.PrintStatistics()
+	// export log to file name in current path
+	if len(args) == 3 {
+		ex, err := os.Executable()
+		if err != nil {
+			panic(err)
+		}
+		exPath := filepath.Dir(ex)
+		fmt.Println(exPath)
+		parser.ExportGachaLog(path.Join(exPath, args[2]))
+	}
 }
