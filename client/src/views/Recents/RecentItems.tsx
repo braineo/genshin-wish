88c0ce96fe2bcad4de8365c824e9e8fddc4f350c
Binbin Ye <phantom2501@gmail.com>
2022-09-11 12:27:24 +0900

    add button to jump to all gacha history

--- client/src/views/Recents/RecentItems.tsx
+++ client/src/views/Recents/RecentItems.tsx
@@ -1,5 +1,7 @@
+import { Button } from 'antd';
 import { WishLog } from 'genshin-wish';
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import ItemList from '../../components/ItemList';
 import { useAxios } from '../../utils/axios';
 import styles from './index.module.less';
@@ -15,6 +17,7 @@ type RecentItemsProps = {
 const Recents: React.FC<RecentItemsProps> = props => {
   const [recentLogs, setRecentLogs] = useState<WishLog[]>([]);
   const client = useAxios();
+  const history = useHistory();
   useEffect(() => {
     const fetchLog = async () => {
       const recent = await client.get<{ data: WishLog[] }>(
@@ -32,9 +35,16 @@ const Recents: React.FC<RecentItemsProps> = props => {
     fetchLog();
   }, []);
 
+  const onSeeAll = useCallback(() => {
+    history.push(`/list/${props.userId}/`);
+  }, [history, props.userId]);
+
   return (
     <div className={styles.recentItems}>
-      {props.title}
+      <div className={styles.title}>
+        <div className={styles.titleText}>{props.title}</div>
+        <Button onClick={onSeeAll}>查看全部</Button>
+      </div>
       <ItemList wishLogs={recentLogs} />
     </div>
   );
